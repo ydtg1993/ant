4773228472969a962d393380fb63a6e9385fe83b
ydtg199303 <946818508@qq.com>
2019-05-21 21:50:25 +0800

    optimize

--- src/structure.php
+++ src/structure.php
@@ -6,5 +6,14 @@ namespace Ant;
 
 class structure
 {
+    const NOTICE_EVENT = "notice";
+    const BROADCAST_EVENT = "broadcast";
+    const REGISTER_EVENT = "register";
+    const LOGOUT_EVENT = "logout";
 
+    public static $information = [
+        "event" => "",
+        "token" => "",
+        "message" => ""
+    ];
 }
